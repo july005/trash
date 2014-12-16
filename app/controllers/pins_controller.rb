2d4fe70f3ef5b45c0c513bb010c2b598ba3e0f9c
ErinUC <erinuc@gmail.com>
2014-12-16 11:36:20 -0500

    changed code in pins controller (added newgirl.html.erb) and in _form.html.erb

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :set_pin, only: [:show, :newgirl, :edit, :update, :destroy]
 
   respond_to :html
 
@@ -8,7 +8,7 @@ class PinsController < ApplicationController
     respond_with(@pins)
   end
 
-  def show
+  def show #ERIN--this is READ
     respond_with(@pin)
   end
 
@@ -22,21 +22,33 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    if @pin.save
+    redirect_to @pin, notice: 'I Made a Page!'
+    else 
+    render action: 'New Page'
+    end
   end
 
   def update
-    @pin.update(pin_params)
-    respond_with(@pin)
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'I edited a page!'
+    else
+      render action: 'Edit page'
+    end
   end
 
   def destroy
     @pin.destroy
+    redirect_to pins_url
+  end
+
+  def newgirl
+    @pin = Pin.new
     respond_with(@pin)
   end
 
   private
+
     def set_pin
       @pin = Pin.find(params[:id])
     end
