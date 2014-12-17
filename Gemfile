f6fb91d212d2bc27817a2dfca4c4b668c5d5b57f
ErinUC <erinuc@gmail.com>
2014-12-17 14:54:25 -0500

    uninstalled aws gem

--- Gemfile
+++ Gemfile
@@ -15,7 +15,6 @@ gem 'devise', '~> 3.4.1'
 gem 'responsive_images'
 gem 'carrierwave'
 gem "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
-gem 'aws-sdk', '~>1.20.0'
 
 group :development, :test do
      gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'www.storiest.com' }
   #note to set this to actual host
 
-  config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-        :bucket => ENV['S3_BUCKET_NAME'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
-    }
   # Above sets Paperclip to upload images to Amazon S3
 end
