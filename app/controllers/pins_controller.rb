db462837fe1786f86e8c7de038865a98fb8bcbad
ErinUC <erinuc@gmail.com>
2014-12-17 22:02:46 -0500

    Added jQuery masonry

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,8 +2,6 @@ class PinsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_pin, only: [:show, :newgirl, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy, :show]
-  
-
   respond_to :html
 
   def index
